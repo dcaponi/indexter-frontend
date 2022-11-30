ff3131570a9d60ec0fcb9a24ed079ca33baaa5ee
Dominick Caponi <d.caponi1@gmail.com>
2022-11-29 16:07:58 -0800

    adds base path to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/frontend/",
   plugins: [svelte()],
   server: {
     port: 8080
