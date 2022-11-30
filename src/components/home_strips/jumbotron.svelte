80d93e09422eb2f2ae45546279fb9d956ceb5563
Dominick Caponi <d.caponi1@gmail.com>
2022-11-29 16:24:02 -0800

    adds autoplay mute to video

--- src/components/home_strips/jumbotron.svelte
+++ src/components/home_strips/jumbotron.svelte
@@ -6,7 +6,10 @@
 <div class="jumbotron">
     <h2 class="strip-title">Supercharge your knowledge base</h2>
     <!-- svelte-ignore a11y-media-has-caption -->
-    <video src={searchDemo} type="video/mp4"/>
+
+    <video autoplay controls muted>
+        <source src={searchDemo} type="video/mp4"/>
+    </video>
     <main>
         <p>
             Indexter unifies the search experience for your entire knowledge base where you spend most of your time. Let Indexter find and bring you the contextually relevant source material to help you make decisions faster and never lose valuable historical context to the documentation void.
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/frontend/",
+  base: "/",
   plugins: [svelte()],
   server: {
     port: 8080
